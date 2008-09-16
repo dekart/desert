2fc05dde5ceed3026cae92da51911d92c18c39dd
DEkart <dekart@Lamptop.(none)>
2008-09-16 15:00:57 +0600

    Disable ActionMailer extension if Rails version is 2.1.0 or above

--- lib/desert/plugin_templates.rb
+++ lib/desert/plugin_templates.rb
@@ -4,7 +4,7 @@ if Desert::VersionChecker.rails_version_is_below_1990?
   require "#{dir}/plugin_templates/1.2.0/action_mailer"
 elsif Desert::VersionChecker.rails_version_is_below_rc2?
   require "#{dir}/plugin_templates/1.99.0/action_mailer"
-else
+elsif Desert::VersionChecker.rails_version_is_below_21?
   require "#{dir}/plugin_templates/2.0.0/action_mailer"
 end
 require "#{dir}/plugin_templates/action_view"
